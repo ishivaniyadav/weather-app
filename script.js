3d77eb6f7edf3ea104a4fb5d3d056c5963a54e73
Shivani Yadav <150349374+ishivaniyadav@users.noreply.github.com>
2025-05-23 11:02:33 +0530

    script.js

--- script.js
+++ script.js
@@ -18,18 +18,32 @@ function getWeather() {
         .catch(error => {
             console.error('Error fetching weather data:', error);
             alert('Error fetching weather data. Please try again later.');
-        });
-}
+        });    
+} 
 
 function displayWeather(data) {
     const weatherInfo = document.getElementById('weatherInfo');
+
     const cityNameElement = document.getElementById('cityName');
     const temperatureElement = document.getElementById('temperature');
     const descriptionElement = document.getElementById('description');
+    const humidityElement = document.getElementById('humidity');
+    const sunriseElement = document.getElementById('sunrise');
+    const sunsetElement = document.getElementById('sunset');
+
+    const kelvinTemp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    
+    const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+    const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
 
     cityNameElement.textContent = data.name;
-    temperatureElement.textContent = `Temperature: ${Math.round(data.main.temp - 273.15)}°C`;
-    descriptionElement.textContent = `Description: ${data.weather[0].description}`;
+    temperatureElement.textContent = `Temperature: ${Math.round(kelvinTemp - 273.15)}°C`;
+    descriptionElement.textContent = `Description: ${description}`;
+    humidityElement.textContent = `Humidity: ${humidity}%`;
+    sunriseElement.textContent = `Sunrise: ${sunriseTime}`;
+    sunsetElement.textContent = `Sunset: ${sunsetTime}`;
 
     weatherInfo.classList.remove('hidden');
     weatherInfo.classList.add('visible');
